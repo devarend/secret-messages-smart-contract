828b7bf4c47c2eeb61614338c47b7dad4673c4c3
devarend <devarend@gmail.com>
2024-02-02 22:12:39 +0100

    added last 6 bits and flag checks

--- src/SecretMessage.ts
+++ src/SecretMessage.ts
@@ -3,10 +3,18 @@ import {
   SmartContract,
   state,
   State,
-  method, Reducer, PublicKey, PrivateKey, Bool, UInt8
-} from 'o1js';
+  method,
+  Reducer,
+  PublicKey,
+  PrivateKey,
+  Bool,
+  UInt8,
+  Gadgets,
+  Provable,
+} from "o1js";
 
-const adminPublicKey = "B62qmSizxA4KPDV9pAa8G9WxiM9A6Z2YsW8SJr26gR2nFnerMuGn9V1"
+const adminPublicKey =
+  "B62qmSizxA4KPDV9pAa8G9WxiM9A6Z2YsW8SJr26gR2nFnerMuGn9V1";
 
 const initial = {
   state: Bool(false),
@@ -26,39 +34,65 @@ export class SecretMessage extends SmartContract {
 
   @method addAddress(address: PublicKey, adminPrivateKey: PrivateKey) {
     const adminKey = this.adminKey.getAndRequireEquals();
-    PublicKey.fromPrivateKey(adminPrivateKey).equals(adminKey).assertEquals(true);
+    PublicKey.fromPrivateKey(adminPrivateKey)
+      .equals(adminKey)
+      .assertEquals(true);
     const actions = this.reducer.getActions({
       fromActionState: Reducer.initialActionState,
     });
 
     let { state: addressExists, actionState } = this.reducer.reduce(
-        actions,
-        Bool,
-        (state: Bool, action: PublicKey) => state.or(action.equals(address)),
-        initial
+      actions,
+      Bool,
+      (state: Bool, action: PublicKey) => state.or(action.equals(address)),
+      initial
     );
 
-    addressExists.assertFalse('Address exists already');
+    addressExists.assertFalse("Address already exists");
     this.reducer.dispatch(address);
   }
 
   @method addMessage(message: Field, eligiblePrivateKey: PrivateKey) {
     const eligibleAddress = PublicKey.fromPrivateKey(eligiblePrivateKey);
-
     const actions = this.reducer.getActions({
       fromActionState: Reducer.initialActionState,
     });
-
     let { state: isAddressEligible } = this.reducer.reduce(
-        actions,
-        Bool,
-        (state: Bool, action: PublicKey) => state.or(action.equals(eligibleAddress)),
-        initial
+      actions,
+      Bool,
+      (state: Bool, action: PublicKey) =>
+        state.or(action.equals(eligibleAddress)),
+      initial
     );
+    isAddressEligible.assertTrue("Address is not eligible");
 
-    isAddressEligible.assertTrue('Address is not eligible');
-    const amountOfMessagesSent = this.amountOfMessagesSent.getAndRequireEquals();
+    const filteredMessage = Gadgets.and(message, Field.from(63), 254);
+    const [sixthBit, fifthBit, fourthBit, thirdBit, secondBit, firstBit] =
+      filteredMessage.toBits(6);
+
+    const firstCheck = Provable.if(
+      firstBit.equals(Bool(true)),
+      secondBit.or(thirdBit).or(fourthBit).or(fifthBit).or(sixthBit),
+      Bool(false)
+    );
+    firstCheck.assertEquals(Bool(false));
+
+    const secondCheck = Provable.if(
+      secondBit.equals(Bool(true)),
+      thirdBit.equals(Bool(true)),
+      Bool(false)
+    );
+    secondCheck.assertEquals(Bool(true));
+
+    const thirdCheck = Provable.if(
+      fourthBit.equals(Bool(true)),
+      fifthBit.or(sixthBit),
+      Bool(false)
+    );
+    thirdCheck.assertEquals(Bool(false));
+
+    const amountOfMessagesSent =
+      this.amountOfMessagesSent.getAndRequireEquals();
     this.amountOfMessagesSent.set(amountOfMessagesSent.add(1));
   }
-
 }
