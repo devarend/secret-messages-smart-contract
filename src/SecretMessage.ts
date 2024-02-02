8027e118cf0d724f9d8dc387bee99c83acd89986
devarend <devarend@gmail.com>
2024-02-02 13:36:20 +0100

    added action and reducer to add addresses and check if address already exists, added example in main.ts

--- src/SecretMessage.ts
+++ src/SecretMessage.ts
@@ -3,15 +3,20 @@ import {
   SmartContract,
   state,
   State,
-  method, Reducer, PublicKey, PrivateKey
+  method, Reducer, PublicKey, PrivateKey, Bool, Struct
 } from 'o1js';
 
 const adminPublicKey = "B62qmSizxA4KPDV9pAa8G9WxiM9A6Z2YsW8SJr26gR2nFnerMuGn9V1"
 
+const initial = {
+  state: Bool(false),
+  actionState: Reducer.initialActionState,
+};
+
 export class SecretMessage extends SmartContract {
   @state(Field) num = State<Field>();
   @state(PublicKey) adminKey = State<PublicKey>();
-  reducer = Reducer({ actionType: Field });
+  reducer = Reducer({ actionType: PublicKey });
 
   init() {
     super.init();
@@ -19,9 +24,22 @@ export class SecretMessage extends SmartContract {
     this.adminKey.set(PublicKey.fromBase58(adminPublicKey));
   }
 
-  @method addAddress(address: Field, adminPrivateKey: PrivateKey) {
+  @method addAddress(address: PublicKey, adminPrivateKey: PrivateKey) {
     const adminKey = this.adminKey.getAndRequireEquals();
     PublicKey.fromPrivateKey(adminPrivateKey).equals(adminKey).assertEquals(true);
+    const actions = this.reducer.getActions({
+      fromActionState: Reducer.initialActionState,
+    });
+
+    let { state: addressExists, actionState } = this.reducer.reduce(
+        actions,
+        Bool,
+        (state: Bool, action: PublicKey) => state.or(action.equals(address)),
+        initial
+    );
+
+    addressExists.assertFalse('Address exists already')
+    this.reducer.dispatch(address);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {
   Field,
   Mina,
   PrivateKey,
-  AccountUpdate,
+  AccountUpdate, PublicKey,
 } from 'o1js';
 
 const useProof = false;
@@ -12,6 +12,8 @@ const Local = Mina.LocalBlockchain({ proofsEnabled: useProof });
 Mina.setActiveInstance(Local);
 const { privateKey: deployerKey, publicKey: deployerAccount } = Local.testAccounts[0];
 const { privateKey: senderKey, publicKey: senderAccount } = Local.testAccounts[1];
+
+console.log(deployerAccount.toBase58())
 
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppAddress = zkAppPrivateKey.toPublicKey();
@@ -27,9 +29,18 @@ const num0 = zkAppInstance.num.get();
 console.log('state after init:', num0.toString());
 
 const txn1 = await Mina.transaction(senderAccount, () => {
-  zkAppInstance.addAddress(Field(9), PrivateKey.fromBase58(""));
+  zkAppInstance.addAddress(PublicKey.fromBase58('B62qoFq6xWugjEe4Gag8QYwuTkjuufC8EWiYsstr3c1CfRctNrRtHEg'), PrivateKey.fromBase58(""));
 });
 await txn1.prove();
 await txn1.sign([senderKey]).send();
+
+
+const txn2 = await Mina.transaction(senderAccount, () => {
+  zkAppInstance.addAddress(PublicKey.fromBase58('B62qoFq6xWugjEe4Gag8QYwuTkjuufC8EWiYsstr3c1CfRctNrRtHEg'), PrivateKey.fromBase58(""));
+});
+await txn2.prove();
+await txn2.sign([senderKey]).send();
+
+
 const num1 = zkAppInstance.num.get();
 console.log('state after txn1:', num1.toString());
